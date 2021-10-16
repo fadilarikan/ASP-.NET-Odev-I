d7939baf4d2da2e90d4d9f34fb27440039ba5a7d
fadilarikan <fadilarikan19664@gmail.com>
2021-10-16 11:13:25 +0300

    Add project files.

---
+++ Odev-I/CustomHTMLHelper/HTMLHelperExtensions.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Html;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace Odev_I.CustomHTMLHelper
+{
+    public static class HTMLHelperExtensions 
+    {
+
+		public static IHtmlContent Date(this IHtmlHelper helper, string type, string name, string id)
+		{
+
+			TagBuilder builder = new TagBuilder("input");
+			builder.Attributes.Add("type", type);
+			builder.Attributes.Add("name", name);
+			builder.Attributes.Add("id", id);
+			builder.TagRenderMode = TagRenderMode.SelfClosing;
+
+			return builder;
+		}
+
+		public static IHtmlContent Button(this IHtmlHelper helper, string type, string text)
+		{
+
+			TagBuilder builder = new TagBuilder("button");
+			builder.Attributes.Add("type", type);
+			builder.Attributes.Add("",text);
+			builder.TagRenderMode = TagRenderMode.EndTag;
+
+			return builder;
+		}
+		public static IHtmlContent Button(this IHtmlHelper helper, string text)
+		{
+			string buttonHtml = $"<button>{text}</button>";
+			HtmlString button = new HtmlString(buttonHtml);
+			return button;
+		}
+
+	}
+}
---
+++ Odev-I/CustomizeTagHelpers/ContentManagementTagHelper.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+
+namespace Odev_I.CustomizeTagHelpers
+{
+	[HtmlTargetElement("contentManage")]
+	public class ContentManagementTagHelper : TagHelper
+    {
+		public string Title { get; set; }
+		public string TitleColor { get; set; }
+		public string Text { get; set; }
+		public string TextColor { get; set; }
+
+
+		public override void Process(TagHelperContext context, TagHelperOutput output)
+		{
+
+			output.TagName = "div";
+			output.Attributes.Add("class", "divContent");
+			output.PreContent.SetHtmlContent($"<h2 style= color:{TitleColor}> {Title}</h2>");
+			output.PostContent.SetHtmlContent($"<p style= color:{TextColor}> {Text}</p2>");
+
+		}
+
+	}
+}
